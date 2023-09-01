9fc99e11e8c3f2abb427c5f74c1d0635798ded14
chaeyunlee00 <chaeyunlee00@gmail.com>
2023-09-01 12:44:37 +0900

    최초커밋

---
+++ src/git_test3/Sort.java
@@ -0,0 +1,19 @@
+package git_test3;
+
+public class Sort {
+	public static void sort(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			int value=a[i];
+			int j;
+			for(j= i-1; j>=0; --j) {
+				if(a[j] > value) {
+					a[j+1] = a[j];
+				}else {
+					break;
+				}
+				
+			}//안쪽 for
+			a[j+1] = value;
+		}//바깥쪽 for
+	}//sort메소드
+}//sort클래스
